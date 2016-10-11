7c57a6a8f6a7497d59691760ca11feed3073841e
Ethan Law and Tam Nguyen <ethan+tam@>
2016-10-11 16:43:33 -0700

    add initial files and begin setup of gulp files

---
+++ js/weather-interface.js
@@ -0,0 +1,12 @@
+var apiKey = "8e9591214649390992f120f95e4b3fa2";
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $('.showWeather').text("The city you have chosen is " + city + ".");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      console.log(response);
+    });
+  });
+});
